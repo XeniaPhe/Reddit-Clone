158138fb10e13346f2182edd316fd247466ce04d
XeniaPhe <m.tunahanbas90@gmail.com>
2024-12-26 13:53:31 +0300

    adding support for more types and ordering, incomplete yet

--- reddit_clone/core/filters/filter.py
+++ reddit_clone/core/filters/filter.py
@@ -4,16 +4,17 @@ from functools import wraps
 from django.db.models.query import QuerySet
 from graphene_django.types import DjangoObjectType
 
-from core.filters.filter_utils import get_graphene_filter_arguments, get_django_filter_arguments
+from core.filters.filter_utils import get_graphene_filter_arguments, get_graphene_orderby_arguments, get_django_filter_arguments
 
 def filtered_list(of_type: Type[DjangoObjectType], **kwargs):
     filter_arguments = get_graphene_filter_arguments(of_type)
-    combined_kwargs = {**kwargs, **filter_arguments}
+    orderby_arguments = get_graphene_orderby_arguments(of_type)
+    combined_kwargs = {**kwargs, **filter_arguments, **orderby_arguments}
     return graphene.List(of_type, **combined_kwargs)
 
 def filter_queryset(graphene_model_type: Type[DjangoObjectType], queryset: QuerySet, **filter_params) -> QuerySet:
     filter_arguments = get_django_filter_arguments(graphene_model_type, **filter_params)
-    return queryset.filter(**filter_arguments)
+    queryset = queryset.filter(**filter_arguments)
 
 def filter(graphene_model_type: Type[DjangoObjectType]):
     def decorator(func):
--- reddit_clone/core/filters/filter_utils.py
+++ reddit_clone/core/filters/filter_utils.py
@@ -10,12 +10,23 @@ from core.custom_errors import filter_error, bad_request
 _django_to_graphene_type_map = {
     models.CharField: graphene.String,
     models.EmailField: graphene.String,
+    models.JSONField: graphene.String,
+    models.FileField: graphene.String,
+    models.IPAddressField: graphene.String,
+    models.GenericIPAddressField: graphene.String,
+    models.UUIDField: graphene.UUID,
     models.IntegerField: graphene.Int,
+    models.DurationField: graphene.Int,
+    models.BigIntegerField: graphene.BigInt,
     models.FloatField: graphene.Float,
     models.DecimalField: graphene.Decimal,
     models.BooleanField: graphene.Boolean,
     models.DateField: graphene.Date,
+    models.TimeField: graphene.Time,
     models.DateTimeField: graphene.DateTime,
+    models.ForeignKey: graphene.ID,
+    models.OneToOneField: graphene.ID,
+    models.ManyToManyField: type(graphene.List(graphene.ID)),
 }
 
 _fixed_type_operator_map = {
@@ -23,10 +34,25 @@ _fixed_type_operator_map = {
     ops.STRING_TAKING_OPERATORS: graphene.String,
     tuple(ops.INTEGER_TAKING_OPERATORS.keys()): graphene.Int,
     ops.DATE_TAKING_OPERATORS: graphene.Date,
+    ops.TIME_TAKING_OPERATORS: graphene.Time,
     ops.DATETIME_TAKING_OPERATORS: graphene.DateTime,
 }
 
-def _get_graphene_field_type(graphene_field: str, model_type: Type[models.Model]) -> Type:
+def _to_original_field_name(graphene_model_type: Type[DjangoObjectType], graphql_field_name: str):
+    original_field_name = to_snake_case(graphql_field_name)
+    django_model_fields = [field.name for field in graphene_model_type._meta.model._meta.get_fields()]
+    
+    #Field name is not snake_case so it changed when it was converted to camelCase and then to snake_case
+    #This should work for camelCase, PascalCase and SCREAMING_SNAKE_CASE variables
+    if original_field_name not in django_model_fields:
+        for django_model_field in django_model_fields:
+            if django_model_field.replace('_', '').lower() == graphql_field_name.lower():
+                original_field_name = django_model_field
+                break
+    
+    return original_field_name
+
+def _get_graphene_field_type(graphene_field: str, model_type: Type[models.Model], suppress_logs=False) -> Type:
     try:
         model_field = model_type._meta.get_field(graphene_field)
     except:
@@ -36,17 +62,23 @@ def _get_graphene_field_type(graphene_field: str, model_type: Type[models.Model]
     for django_type, graphene_type in _django_to_graphene_type_map.items():
         if isinstance(model_field, django_type):
             return graphene_type
-        
-    print(f'Could not find the type of "{model_field}" in recognized types, defaulting to "graphene.String"')
+    
+    if not suppress_logs:
+        print(f'Could not find the type of "{model_field}" in recognized types, defaulting to "graphene.String"')
+    
     return graphene.String
 
 def _get_graphene_argument_type(filter_operator: str, graphene_field_type: Type) -> Type:
-    is_numeric = graphene_field_type in (graphene.Int, graphene.Float, graphene.Decimal)
+    is_numeric = graphene_field_type in (graphene.Int, graphene.BigInt, graphene.Float, graphene.Decimal)
+    is_id = graphene_field_type in (graphene.ID, graphene.UUID)
     is_invalid = (is_numeric and filter_operator not in ops.NUMERIC_OPERATORS)
+    is_invalid |= (is_id and filter_operator not in ops.ID_OPERATORS)
     is_invalid |= (graphene_field_type == graphene.String and filter_operator not in ops.STRING_OPERATORS)
     is_invalid |= (graphene_field_type == graphene.Boolean and filter_operator not in ops.BOOLEAN_OPERATORS)
     is_invalid |= (graphene_field_type == graphene.Date and filter_operator not in ops.DATE_OPERATORS)
+    is_invalid |= (graphene_field_type == graphene.Time and filter_operator not in ops.TIME_OPERATORS)
     is_invalid |= (graphene_field_type == graphene.DateTime and filter_operator not in ops.DATETIME_OPERATORS)
+    is_invalid |= (graphene_field_type == graphene.List and filter_operator not in ops.LIST_OPERATORS)
     
     if is_invalid:
         filter_error(f'Invalid filter operator "{filter_operator}" for field type {graphene_field_type.__name__}')
@@ -54,7 +86,7 @@ def _get_graphene_argument_type(filter_operator: str, graphene_field_type: Type)
     for fixed_type_operators, graphene_type in _fixed_type_operator_map.items():
         if filter_operator in fixed_type_operators:
             return graphene_type
-        
+    
     if filter_operator in ops.COMPARISON_OPERATORS:
         return graphene_field_type
     elif filter_operator == ops.IN:
@@ -93,12 +125,28 @@ def get_graphene_filter_arguments(graphene_model_type: Type[DjangoObjectType]):
             argument_type = _get_graphene_argument_type(filter_operator, graphene_field_type)
             argument = graphene.Argument(argument_type, name=graphql_filter, required=False)
             filter_arguments[graphql_filter] = argument
-    
+
     return filter_arguments
 
+def get_graphene_orderby_arguments(graphene_model_type: Type[DjangoObjectType]):
+    django_model_type = graphene_model_type._meta.model
+    graphene_fields = graphene_model_type._meta.fields
+    
+    ordering_choices = {}
+    
+    for ordered_field in graphene_fields:
+        _get_graphene_field_type(ordered_field, django_model_type, True)
+        graphql_field = to_camel_case(ordered_field)
+        
+        ordering_choices[graphql_field] = graphql_field
+        ordering_choices[f'-{graphql_field}'] = f'-{graphql_field}'
+        
+    enum_type = type(f'{django_model_type.__name__}OrderByEnum', (graphene.Enum,), ordering_choices)
+    
+    return {'orderBy': graphene.Argument(enum_type, name='orderBy', required=False)}
+        
 def get_django_filter_arguments(graphene_model_type: Type[DjangoObjectType], **graphql_filter_params):
     all_filters = get_graphene_filter_arguments(graphene_model_type).keys()
-    graphene_fields = graphene_model_type._meta.fields
     filter_arguments = {}
     
     for graphql_filter, filter_value in graphql_filter_params.items():
@@ -106,15 +154,7 @@ def get_django_filter_arguments(graphene_model_type: Type[DjangoObjectType], **g
             continue
         
         graphql_filter_parts = graphql_filter.split('_')
-        filtered_field = to_snake_case(graphql_filter_parts[0])
-        
-        #Field name is not snake_case so it changed when it was converted to camelCase and then to snake_case
-        if filtered_field not in graphene_fields:
-            for graphene_field in graphene_fields:
-                if graphene_field.lower() == graphql_filter_parts[0].lower():
-                    filtered_field = graphene_field
-                    break
-        
+        filtered_field = _to_original_field_name(graphene_model_type, graphql_filter_parts[0])
         filter_operator = '_'.join(graphql_filter_parts[1:]).lower()
         filter_operator = filter_operator if filter_operator != '' else ops.EXACT
         
@@ -134,3 +174,11 @@ def get_django_filter_arguments(graphene_model_type: Type[DjangoObjectType], **g
         filter_arguments[django_filter] = filter_value
     
     return filter_arguments
+
+def get_django_orderby_argument(graphene_model_type: Type[DjangoObjectType], **graphql_filter_params):
+    orderby = graphql_filter_params.get('orderBy', None)
+    if orderby:
+        is_descending = True if orderby[0] == '-' else False
+        original_field_name = _to_original_field_name(graphene_model_type, orderby.strip('-'))
+        return f'-{original_field_name}' if is_descending else original_field_name
+    return None
