6adfe9bd0112dc008375d176d566fc0c51864020
XeniaPhe <m.tunahanbas90@gmail.com>
2024-12-31 14:53:50 +0300

    changed the structure of comments and posts making use of a third table called contents which I connect to comments and posts table as a one-to-one composition relationsship, tried to make it work with MTI, STI and abstract base classes along with generic foreign keys for a while but settled for composition instead

--- reddit_clone/core/admin.py
+++ reddit_clone/core/admin.py
@@ -5,5 +5,7 @@ from core.models import *
 admin.site.register(User)
 admin.site.register(Community)
 admin.site.register(Membership)
+admin.site.register(Content)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Vote)
--- reddit_clone/core/models.py
+++ reddit_clone/core/models.py
@@ -6,7 +6,7 @@ from core.managers import UserManager
 from core.auth.roles import MEMBER, DB_ROLE_CHOICES
 
 class User(AbstractBaseUser):
-    username = models.CharField(max_length=64, primary_key=True)
+    username = models.CharField(max_length=48, primary_key=True)
     email = models.EmailField(unique=True)
     join_date = models.DateField(auto_now_add=True)
     score = models.IntegerField(default=0)
@@ -32,8 +32,9 @@ class User(AbstractBaseUser):
         return self.is_superuser
     
 class Community(models.Model):
-    name = models.CharField(max_length=64, primary_key=True)
+    name = models.CharField(max_length=48, primary_key=True)
     description = models.TextField(blank=True)
+    users = models.ManyToManyField(to=User, through='Membership', related_name='communities')
     
     def __str__(self):
         return self.name
@@ -45,29 +46,65 @@ class Membership(models.Model):
     community = models.ForeignKey(to=Community, on_delete=models.CASCADE, related_name='memberships')
     
     def __str__(self):
-        return f'{self.community}-{self.user}'
+        return f'{self.community[:24]}-{self.user[:23]}'
     
     class Meta:
         unique_together = [['user', 'community']]
 
-class Post(models.Model):
+class Content(models.Model):
+    class ContentType(models.IntegerChoices):
+        POST = 0, 'Post'
+        COMMENT = 1, 'Comment'
+    
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=128)
-    body = models.TextField(blank=True)
+    body = models.TextField(blank=True, default='')
     publish_date = models.DateTimeField(auto_now_add=True)
+    content_type = models.IntegerField(choices=ContentType.choices)
+    
+    def is_comment(self):
+        return self.content_type == Content.ContentType.COMMENT
+    
+    def is_post(self):
+        return self.content_type == Content.ContentType.POST
+    
+    def get_related_object(self):
+        manager = Post.objects if self.is_post() else Comment.objects
+        return manager.get(id=self.id)
+    
+    def __str__(self):
+        return self.body[:48]
+
+def get_post_content():
+    return Content.objects.create(content_type=Content.ContentType.POST)
+
+def get_comment_content():
+    return Content.objects.create(content_type=Content.ContentType.COMMENT)
+
+class Post(models.Model):
+    content = models.OneToOneField(to=Content, on_delete=models.CASCADE, primary_key=True, default=get_post_content)
+    title = models.CharField(max_length=96)
     user = models.ForeignKey(to=User, on_delete=models.DO_NOTHING, related_name='posts')
     community = models.ForeignKey(to=Community, on_delete=models.CASCADE, related_name='posts')
     
     def __str__(self):
-        return self.title
-    
+        return self.title[:48]
+
 class Comment(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    body = models.TextField(blank=True)
-    publish_date = models.DateTimeField(auto_now_add=True)
+    content = models.OneToOneField(to=Content, on_delete=models.CASCADE, primary_key=True, default=get_comment_content)
+    parent = models.ForeignKey(to=Content, on_delete=models.DO_NOTHING, related_name='children')
     user = models.ForeignKey(to=User, on_delete=models.DO_NOTHING, related_name='comments')
-    parent = models.ForeignKey(to='self', on_delete=models.DO_NOTHING, related_name='children')
     post = models.ForeignKey(to=Post, on_delete=models.DO_NOTHING, related_name='comments')
-    
-    def __str__(self):
-        return f'{self.post}-{self.user}-{self.id}'
+
+class Vote(models.Model):
+    class VoteType(models.IntegerChoices):
+        DOWNVOTE = -1, 'Downvote'
+        NONVOTE = 0, 'None'
+        UPVOTE = 1, 'Upvote'
+        
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    vote = models.IntegerField(choices=VoteType.choices, default=VoteType.NONVOTE)
+    user = models.ForeignKey(to=User, on_delete=models.DO_NOTHING, related_name='votes')
+    content = models.ForeignKey(to=Content, on_delete=models.DO_NOTHING, related_name='votes')
+
+    class Meta:
+        unique_together = [['user', 'content']]
